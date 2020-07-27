8bab0618bac049e77c1d7dfa0c49e364a3fb2f00
XuNan <41865662@qq.com>
2020-07-27 09:22:48 +0800

    Update server.js

--- week04/practice/server.js
+++ week04/practice/server.js
@@ -1,17 +1,19 @@
-const http = require('http')
+const http = require('http'); 
 
-http.createServer((request,response)=>{
-    let body=[];
-    request.on('error',(err)=>{
-        console.error(err);
-    }).on('data',(chunk)=>{
-        body.push(chunk.toString());
-    }).on('end',()=>{
-        body=Buffer.contat(body).toString();
-        console.log("body:",body);
-        response.writeHead(200,{'Content-Type':'text/html'});
-        response.end('<html><body>Hello World</body></html>\n');
+http.createServer((request, response) => {
+  let body = [];
+  request.on('error', (err) => {
+    console.error(err);
+  }).on('data', (chunk) => {
+    console.log("chunk:" + chunk.toString());
+    body.push(chunk.toString());
+  }).on('end', () => { 
+    body = Buffer.concat(body).toString();
+    response.writeHead(200, {
+      'Content-Type': 'text/html'
     });
+    response.end('<html><body>Hello World</body></html>\n');
+  });
 
 }).listen(8088);
 
